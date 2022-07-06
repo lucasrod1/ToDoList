0cf6c9cdf7081e306a5b8e8a305f43bb1534ff24
lucasrod1 <lucasfarina97@gmail.com>
2022-07-05 23:01:55 -0300

    Se crearon las funciones basicas para los eventos de la lista de tareas

--- app.js
+++ app.js
@@ -17,3 +17,34 @@ document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("click", filterTodo);
+
+// Functions
+function addTodo(event){
+    event.preventDefault();
+
+    //To Do div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Add to do to LocalStorage
+    saveLocalTodos(todoInput.value);
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoButton.appendChild(completedButton);
+    //Check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+    //Clear todo input value
+    todoInput.value = '';
+}
+
